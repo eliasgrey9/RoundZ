5b54d6e431122cab40a5bfc8075c7f38da4eef0a
Elias Grey <eliasgrey@Isaiahs-MacBook-Pro.local>
2023-03-08 10:20:05 -0800

    revamped signIn and signUp css and structure

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,47 +1,34 @@
-import React, {useState} from "react";
-import style from './signIn.module.css'
-import Navbar from './Navbar/Navbar'
-import backArrow from '../../assets/backArrow.svg'
-import Footer from "../Footer/Footer"
+import React from "react";
+import style from "./signIn.module.css";
 import { Link } from "react-router-dom";
 
-
 const SignIn = () => {
-
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [confirmEmail, setConfirmEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Handle form submission here
-  };
-
   return (
-  <div className={style.body}>
-    <Navbar />
+    <div className={style.body}>
+      <div className={style.logo}>Roundz</div>
 
-    <div className={style.backArrow}><Link to={'/'}><img src={backArrow}></img></Link></div>
-    <form className={style.form} onSubmit={handleSubmit}>
-      <label className={style.formLabel}>
-      Username:
-      </label>
-        <input className={style.formInput} type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      <br />
+      <form className={style.form}>
+        <label className={style.label}>Email</label>
+        <input
+          className={style.input}
+          type="text"
+          placeholder="Enter your email"
+        ></input>
 
-      <label className={style.formLabel}>
-        Password:
-        </label>
-        <input className={style.formInput} type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <br/>
-     
-      <button className={style.submitBtn} type="submit">Sign In</button>
-    </form>
-    <Footer />
-  </div>
-)};
+        <label className={style.label}>Password</label>
+        <input
+          className={style.input}
+          type="password"
+          placeholder="Password"
+        ></input>
+        <input
+          className={style.submitBtn}
+          type="submit"
+          value="Sign In"
+        ></input>
+      </form>
+    </div>
+  );
+};
 
-  
 export default SignIn;
--- src/components/Signup/SignUp.js
+++ src/components/Signup/SignUp.js
@@ -1,66 +1,50 @@
-import React, {useState} from "react";
-import style from './signUp.module.css'
-import Navbar from './Navbar/Navbar'
-import backArrow from '../../assets/backArrow.svg'
-import Footer from "../Footer/Footer"
+import React from "react";
+import style from "./signUp.module.css";
 import { Link } from "react-router-dom";
 
-
 const SignUp = () => {
-
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [confirmEmail, setConfirmEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Handle form submission here
-  };
-
   return (
-  <div className={style.body}>
-    <Navbar />
+    <div className={style.body}>
+      <div className={style.logo}>Roundz</div>
+      <div className={style.subHeading}>Sign up with RoundZ for free</div>
 
-    <div className={style.backArrow}><Link to={'/'}><img src={backArrow}></img></Link></div>
-    <form className={style.form} onSubmit={handleSubmit}>
-      <label className={style.formLabel}>
-      Username:
-      </label>
-        <input className={style.formInput} type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      <br />
-
-      <label className={style.formLabel}>
-        Email:
+      <form className={style.form}>
+        <label className={style.label}>Email</label>
+        <input
+          className={style.input}
+          type="text"
+          placeholder="Enter your email"
+        ></input>
+        <label className={style.label}>Enter your full name</label>
+        <input
+          className={style.input}
+          type="text"
+          placeholder="Jane Smith"
+        ></input>
+        <label className={style.label}>
+          Choose a password with at least 8 characters
         </label>
+        <input
+          className={style.input}
+          type="password"
+          placeholder="Password"
+        ></input>
+        <input
+          className={style.submitBtn}
+          type="submit"
+          value="Sign Up"
+        ></input>
+        <div className={style.alreadyHaveAnAccount}>
+          Already have an account?{" "}
+          <div>
+            <Link className={style.logIn} to={"/signIn"}>
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-        <input className={style.formInput} type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <br />
-      <label className={style.formLabel}>
-        Confirm Email:
-        </label>
-
-        <input className={style.formInput} type="email" value={confirmEmail} onChange={(e) => setConfirmEmail(e.target.value)} />
-      <br />
-
-      <label className={style.formLabel}>
-        Password:
-        </label>
-
-        <input className={style.formInput} type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <br />
-       <label className={style.formLabel}>
-        Confirm Password:
-        </label>
-
-        <input className={style.formInput} type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-      <br />
-      <button className={style.submitBtn} type="submit">Sign Up</button>
-    </form>
-    <Footer />
-  </div>
-)};
-
-  
 export default SignUp;
