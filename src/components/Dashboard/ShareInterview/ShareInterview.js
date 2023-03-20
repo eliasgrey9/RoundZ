2d2cd4f01eafcba99c703ae58119444bf47ed91c
Elias Grey <eliasgrey@Isaiahs-MacBook-Pro.local>
2023-03-20 12:25:02 -0700

    bulk email ability added

--- src/components/Dashboard/ShareInterview/ShareInterview.js
+++ src/components/Dashboard/ShareInterview/ShareInterview.js
@@ -4,13 +4,14 @@ import axios from "axios";
 import style from "./shareInterview.module.css";
 import { useParams } from "react-router-dom";
 const emailjsKey = process.env.REACT_APP_EMAILJS_KEY;
+const uuidv4 = require("uuid/v4");
 
 const ShareInterview = () => {
   const [formData, setFormData] = useState({ email: "", name: "" });
   const [linkSent, setLinkSent] = useState(false);
   const [randomString, setRandomString] = useState("");
   const [dataFromApi, setDataFromApi] = useState({});
-
+  const [bulkEmailArray, setBulkEmailArray] = useState([]);
   const myForm = useRef();
   const params = useParams();
 
@@ -42,16 +43,24 @@ const ShareInterview = () => {
   //Then updates invitations, generates a new string, and Clears formData
   const handleSubmit = (e) => {
     e.preventDefault();
+    bulkEmailArray.map((e) => {
+      let newRandomStr = "";
+      const charactersLength = characters.length;
+      for (let i = 0; i < 15; i++) {
+        newRandomStr += characters.charAt(
+          Math.floor(Math.random() * charactersLength)
+        );
+      }
+      newRandomStr.toLowerCase();
 
-    const dynamicLink = `http://localhost:3000/interview?inviteId=${randomString}&positionId=${params.id}`;
+      const dynamicLink = `http://localhost:3000/interview?inviteId=${newRandomStr}&positionId=${params.id}`;
+      const currentEmail = e.email;
+      const currentName = e.name;
 
-    
-    if (formData.email !== "" && formData.name !== "") {
       emailjs.init(emailjsKey);
-
       emailjs
         .send("service_roundz", "template_roundz", {
-          to_email: formData.email,
+          to_email: currentEmail,
           from_name: "Elias's Test Name",
           from_email: "eliasgrey9@gmail.com",
           message: `Invite link: ${dynamicLink}`,
@@ -61,9 +70,9 @@ const ShareInterview = () => {
             axios.post(
               `http://localhost:8080/api/jobs/addCandidateToPosition/${params.id}`,
               {
-                name: formData.name,
-                email: formData.email,
-                code: randomString,
+                name: currentName,
+                email: currentEmail,
+                code: newRandomStr,
               }
             );
           }
@@ -84,15 +93,12 @@ const ShareInterview = () => {
         .catch(function (error) {
           console.error("Failed to send email: " + error);
         });
-    } else {
-      return;
-    }
+    });
   };
 
   //Sets the values of the input fields for the handleSubmit fn to grab
   const handleChange = (event) => {
     setFormData({ ...formData, [event.target.name]: event.target.value });
-    generateString(15);
   };
 
   const updateInvitations = (response) => {
@@ -113,27 +119,47 @@ const ShareInterview = () => {
     renderSinglePosition();
   }, [params.id]);
 
-  console.log("formData", formData);
+  console.log("BulkEmailArray", bulkEmailArray);
+
+  const updateEmailArray = (e) => {
+    e.preventDefault();
+    if (formData.email !== "" && formData.name !== "") {
+      const updateEmailArray = [...bulkEmailArray];
+      updateEmailArray.push({ name: formData.name, email: formData.email });
+      setBulkEmailArray(updateEmailArray);
+      setFormData({ email: "", name: "" });
+    }
+  };
 
   return (
-    <form ref={myForm} onSubmit={handleSubmit}>
-      {linkSent && <p>Link sent to your email!</p>}
-      <input
-        type="text"
-        name="name"
-        placeholder="Recipients name"
-        value={formData.name}
-        onChange={handleChange}
-      />
-      <input
-        type="email"
-        name="email"
-        placeholder="Recipients email"
-        value={formData.email}
-        onChange={handleChange}
-      />
-      <button type="submit">Send Invite</button>
-    </form>
+    <>
+      <form ref={myForm} onSubmit={updateEmailArray}>
+        {linkSent && <p>Link sent to your email!</p>}
+        <input
+          type="text"
+          name="name"
+          placeholder="Recipients name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Recipients email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <button type="submit">+</button>
+        <button onClick={handleSubmit}>Send invites</button>
+      </form>
+      <div>
+        {bulkEmailArray.map((e) => (
+          <div key={uuidv4()}>
+            Name:{e.name}Email{e.email}
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
